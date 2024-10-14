80ba6e3c7d972d41bfc650c754920b54e5bdb6e2
mingzhaoliang <timothymc.leung@gmail.com>
2024-10-14 20:12:32 +1100

    :art: frontend - apply remix-flat-routes to organise route files

--- frontend/app/routes/_auth/_layout.tsx
+++ frontend/app/routes/_auth/_layout.tsx
@@ -1,7 +1,7 @@
 import Mark from "@/components/common/mark";
 import { Link, Outlet } from "@remix-run/react";
 
-export default function Auth() {
+export default function AuthLayout() {
   return (
     <div className="w-screen h-screen pt-32 flex flex-col items-center">
       <Link to="/" className="-ml-4">
