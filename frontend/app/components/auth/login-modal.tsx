7c2efa6ad89940002875b6b6227c19cbdca49d47
mingzhaoliang <timothymc.leung@gmail.com>
2024-11-05 23:40:42 +1100

    :hammer: frontend - update LoginModal props

--- frontend/app/components/auth/login-modal.tsx
+++ frontend/app/components/auth/login-modal.tsx
@@ -1,16 +1,18 @@
 import LoginForm from "@/routes/_auth/login";
 import { useState } from "react";
 import Mark from "../shared/mark";
-import { ResponsiveDialog } from "../ui/responsive-dialog";
+import { ResponsiveDialog, ResponsiveDialogChildren } from "../ui/responsive-dialog";
 
-export default function LoginModal({ children }: { children: React.ReactNode }) {
+export default function LoginModal(
+  props: React.ComponentPropsWithoutRef<ResponsiveDialogChildren["Trigger"]>
+) {
   const [open, setOpen] = useState(false);
 
   return (
     <ResponsiveDialog query="(min-width: 768px)" modal open={open} onOpenChange={setOpen}>
       {({ Header, Title, Content, Description, Trigger }) => (
         <>
-          <Trigger className="flex-center">{children}</Trigger>
+          <Trigger {...props} />
           <Content className="responsive-dialog-content md:w-[420px] flex flex-col items-center px-4 pb-6 md:py-10">
             <Header>
               <Title className="-ml-4 mb-6">
